be422211001ef78f33bb2c967a34d9ec1ba78336
visotnik <tet129@gmail.com>
2022-06-02 14:00:55 +0300

    index.js update to react 18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
+// React 17
 import ReactDom from "react-dom"
+// React 18
+import * as ReactDOMClient from 'react-dom/client';
 
-const element = <h1>Hello world!</h1>
-
+const element = <h1>Hello world / React 18!</h1>
 console.log (element)
+
+const container = document.getElementById("root")
+
+// React 17
+// ReactDom.render(element, container)
+
+// React 18
+const root = ReactDOMClient.createRoot(container)
+root.render(element)
